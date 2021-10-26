2522c8048e3d687c10c8b69ee99296dfbbf00825
Phelps <phelpsmemo@gmail.com>
2021-10-26 20:37:51 -0300

    move generated angles from input to a specific output directory

--- main.py
+++ main.py
@@ -2,21 +2,32 @@ import os
 import sys
 from pathlib import Path
 
-from osgeo import gdal
-
-
 if len(sys.argv) < 2:
     print('missing args, use SCENEID')
     sys.exit()
 
+#
+# Defining the working directory
+#
 work_dir = Path(f'/work/{sys.argv[1]}')
 work_dir.mkdir(parents=True, exist_ok=True)
 os.chdir(work_dir)
 
+#
+# Generating bands
+#
 os.system(f'/app/l8_angles/l8_angles /mnt/input-dir/{sys.argv[1]}/{sys.argv[1]}_ANG.txt BOTH 1 -b 4')
 
-#export each band
-os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 1 {work_dir}/{sys.argv[1]}_sensor_B04.img /mnt/input-dir/{sys.argv[1]}/{sys.argv[1]}_VAA.tif')
-os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 2 {work_dir}/{sys.argv[1]}_sensor_B04.img /mnt/input-dir/{sys.argv[1]}/{sys.argv[1]}_VZA.tif')
-os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 1 {work_dir}/{sys.argv[1]}_solar_B04.img /mnt/input-dir/{sys.argv[1]}/{sys.argv[1]}_SAA.tif')
-os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 2 {work_dir}/{sys.argv[1]}_solar_B04.img /mnt/input-dir/{sys.argv[1]}/{sys.argv[1]}_SZA.tif')
+#
+# Creating the output directory
+#
+output_directory = f"/mnt/output-dir/{sys.argv[1]}"
+os.makedirs(output_directory, exist_ok=True)
+
+#
+# Exporting each band
+#
+os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 1 {work_dir}/{sys.argv[1]}_sensor_B04.img {output_directory}/{sys.argv[1]}_VAA.tif')
+os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 2 {work_dir}/{sys.argv[1]}_sensor_B04.img {output_directory}/{sys.argv[1]}_VZA.tif')
+os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 1 {work_dir}/{sys.argv[1]}_solar_B04.img {output_directory}/{sys.argv[1]}_SAA.tif')
+os.system(f'gdal_translate -co "COMPRESS=DEFLATE" -b 2 {work_dir}/{sys.argv[1]}_solar_B04.img {output_directory}/{sys.argv[1]}_SZA.tif')
